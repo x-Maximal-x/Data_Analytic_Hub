1a8dd5ddc0c15a228a8699940a8712f017f32b29
x-Maximal-x <s4000728@student.rmit.edu.au>
2023-10-17 20:56:47 +0530

    Sign-up page functions

--- Data_Analytic_Hub/src/application/SignUpController.java
+++ Data_Analytic_Hub/src/application/SignUpController.java
@@ -1,11 +1,31 @@
 package application;
 
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
 
 public class SignUpController implements Initializable {
 	
-	@FXMl
+	@FXML
 	private TextField username;
+	
+	@FXML
+	private TextField password;
+	
+	@FXML
+	private TextField firstname;
+	
+	@FXML
+	private TextField lastname;
+	
+	@
+	
 	
 	
 
--- Data_Analytic_Hub/src/application/loginController.java
+++ Data_Analytic_Hub/src/application/loginController.java
@@ -4,7 +4,6 @@ package application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
@@ -19,7 +18,6 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ResourceBundle;
 
-import javafx.scene.Parent;
 import javafx.scene.control.Alert;
 
 public class loginController implements Initializable {
