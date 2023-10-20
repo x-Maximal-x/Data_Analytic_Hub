83c9f8351ed40f2918ecf454d9febbbaeb14fe38
x-Maximal-x <s4000728@student.rmit.edu.au>
2023-10-20 18:50:57 +0530

    Added ReadMe file

---
+++ Data_Analytic_Hub/README.txt
@@ -0,0 +1,30 @@
+Data Analytics Hub
+
+IDE: We developed this project using Eclipse IDE
+
+Java Version: The project was built using JavaSE 20.
+
+JavaFX: This project uses JavaFX-17.0.8 for the graphical user interface.
+
+MySQL Database: The project connects to a MySQL Version 8.0.34 database for user authentication . 
+
+
+Installation and Running the Code
+
+
+Clone this repository to your local machine or download the source code as a ZIP file and extract it.
+
+
+
+Update the database connection details in the DBUtils.java file to match your MySQL configuration.
+Make connection using the "jdbc:mysql://localhost:3307/users", "root", "admin" 
+configure the database to match the requirement 
+
+Setup login Table with column user_id, username, paswword, firstname and lastname .
+
+Open the project folder using your preferred Java IDE (e.g., Eclipse).
+Configure JavaFX dependencies
+
+In Eclipse, you can do this by adding the JavaFX libraries to your project's build path.
+
+Locate the Main.java file in the project directory and run it as a Java application. This will start the Data Analytics Hub application.
--- Data_Analytic_Hub/src/application/Main.java
+++ Data_Analytic_Hub/src/application/Main.java
@@ -1,5 +1,5 @@
 package application;
-	
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import javafx.application.Application;
@@ -8,38 +8,37 @@ import javafx.scene.Scene;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 
-
 public class Main extends Application {
-	@Override
-	public void start(Stage primaryStage)throws Exception {
-		try {
-			Parent root = FXMLLoader.load(Main.class.getResource("/application/login.fxml"));
-			primaryStage.setTitle("Data Analytics Hub");
-			
-			primaryStage.setScene(new Scene(root, 600,400));
-			primaryStage.show();
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
-	}
-	
-	public static void main(String[] args) {
-		
-		
-		Connection con = null;
-		try {
-			con = (Connection)DriverManager.getConnection("jdbc:mysql://localhost:3307/users", "root", "admin");
-			
-			if(con!=null) {
-				System.out.println("COnnected");
-				
-			}
-			
-		}
-		catch(Exception e) {
-			System.out.println("Not connected");
-		}
-		launch(args);
-		
-	}
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        try {
+            // Load the login.fxml file using FXMLLoader
+            Parent root = FXMLLoader.load(Main.class.getResource("/application/login.fxml"));
+            primaryStage.setTitle("Data Analytics Hub");
+
+            // Set the scene with the loaded FXML content
+            primaryStage.setScene(new Scene(root, 600, 400));
+            primaryStage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        Connection con = null;
+        try {
+            // Attempt to establish a connection to the MySQL database
+            con = (Connection) DriverManager.getConnection("jdbc:mysql://localhost:3307/users", "root", "admin");
+
+            if (con != null) {
+                System.out.println("Connected");
+            }
+        } catch (Exception e) {
+            // Handle connection failure
+            System.out.println("Not connected");
+        }
+
+        // Launch the JavaFX application
+        launch(args);
+    }
 }
